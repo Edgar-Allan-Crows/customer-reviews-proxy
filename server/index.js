3f4505582604666df52237b9dd64876d5662579b
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-06 21:25:49 -0400

    Added HPM middleware to route http requests appropriately. App is rendering to browser via proxy server as expected.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// // Debugging
+// app.use((req, res, next) => {
+//   console.log(`The method is: ${req.method}. The url is: ${req.url}`);
+//   next();
+// });
 
 app.use(express.static(__dirname + '/../dist'));
+
+// Redirect http requests here
+app.use('/api/reviews', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/api/totalScore', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/api/reviewCount', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port http://localhost:${port}`);
