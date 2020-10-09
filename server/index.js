b8dda9c155717c932d6c6b328d1a460d071c21b5
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-09 17:38:49 -0400

    Update HPM to account for product_ids in review service

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/../dist'));
 app.use('/gallery/:product_id/:metal/:cut/:carat', createProxyMiddleware({ target: 'http://127.0.0.1:3030/', changeOrigin: true }));
 
 // Redirect customer reviews http requests here
-app.use('/api/reviews', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
-app.use('/api/totalScore', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
-app.use('/api/reviewCount', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/api/totalScore/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/api/reviewCount/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port http://localhost:${port}`);
