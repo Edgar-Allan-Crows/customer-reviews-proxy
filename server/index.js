8fb40c2eba4359e0b3e9a6cf09018669d827495f
Ubuntu <ubuntu@ip-172-31-95-240.ec2.internal>
2020-10-11 01:04:24 +0000

    Added correct IP addresses and ports

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3005;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // // Debugging
@@ -12,13 +12,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 app.use(express.static(__dirname + '/../dist'));
 
 // Redirect product image http requests here
-app.use('/gallery/:product_id/:metal/:cut/:carat', createProxyMiddleware({ target: 'http://127.0.0.1:3030/', changeOrigin: true }));
+app.use('/gallery/:product_id/:metal/:cut/:carat', createProxyMiddleware({ target: 'http://18.216.180.46:3030/', changeOrigin: true }));
 
 // Redirect customer reviews http requests here
-app.use('/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
-app.use('/api/totalScore/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
-app.use('/api/reviewCount/:product_id', createProxyMiddleware({ target: 'http://127.0.0.1:3004/', changeOrigin: true }));
+app.use('/:product_id', createProxyMiddleware({ target: 'http://34.228.73.56:3000/', changeOrigin: true }));
+app.use('/api/totalScore/:product_id', createProxyMiddleware({ target: 'http://34.228.73.56:3000/', changeOrigin: true }));
+app.use('/api/reviewCount/:product_id', createProxyMiddleware({ target: 'http://34.228.73.56:3000/', changeOrigin: true }));
 
 app.listen(port, () => {
-  console.log(`Proxy server listening on port http://localhost:${port}`);
+  console.log(`Proxy server listening on port http://34.228.73.56:${port}`);
 });
